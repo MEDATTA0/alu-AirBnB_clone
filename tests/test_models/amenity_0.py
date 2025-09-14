7396d6043f9a9a28ada2cdeabbf4865be628df4e
blessiingab] <b.ingabire1@alustudent.com>
2025-09-14 19:23:16 +0200

    file

--- tests/test_models/amenity_0.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Test file for Amenity class"""
-from models.amenity import Amenity
-
-amenity = Amenity()
-print("OK" if type(amenity) is Amenity else "Fail")
--- tests/test_models/state_0.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Test file for State class"""
-from models.state import State
-
-state = State()
-print("OK" if type(state) is State else "Fail")
