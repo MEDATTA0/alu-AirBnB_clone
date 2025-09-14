bcfbd0d35303a775b0906eab46b1694bf99cdff1
blessiingab] <b.ingabire1@alustudent.com>
2025-09-14 19:58:13 +0200

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,72 +1,54 @@
-import models
-from uuid import uuid4
+#!/usr/bin/python3
+"""
+BaseModel class definition
+"""
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
     """
-    BaseModel defines common attributes/methods for other classes.
-
-    Attributes:
-        id (str): Unique identifier for each instance.
-        created_at (datetime): Timestamp of instance creation.
-        updated_at (datetime): Timestamp of last update.
+    BaseModel class that defines all common attributes/methods for other
+    classes
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance.
-        If kwargs is empty, generates new id and timestamps.
-        If kwargs is provided, sets attributes from kwargs.
+        Initialize BaseModel instance
         """
-        if len(kwargs) == 0:
-            self.id = str(uuid4())
+        if kwargs:  # case: creating from a dictionary
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+            # Add to storage for instances created from dictionary
+            from models import storage
+            storage.new(self)
+        else:  # case: creating a brand new instance
+            self.id = str(uuid.uuid4())  # unique ID
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # print(self.to_dict())
-            return
-        for key, value in kwargs.items():
-            if key == "__class__":
-                continue
-            if (key in ["created_at", "updated_at"]):
-                try:
-                    casted_time = datetime.fromisoformat(value)
-                    setattr(self, key, casted_time)
-                    continue
-                except:
-                    raise Exception(f"date time is not iso format")
-
-            if key == "id":
-                setattr(self, key, str(value))
-            else:
-                setattr(self, key, value)
+            # Add to storage for new instances
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
-        """
-        Returns a string representation of the instance.
-        Format: [ClassName] (id) {attribute_dict}
-        """
+        """String representation of the instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Updates the updated_at timestamp to current time and saves the instance to storage.
-        """
-        from models import storage
+        """Updates the public instance attribute updated_at with current
+        datetime"""
         self.updated_at = datetime.now()
-        storage.new(self)
+        from models import storage
         storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary of the instance attributes.
-        Converts datetime attributes to ISO format strings.
-        """
-        obj = {}
-        for key, value in self.__dict__.items():
-            if key in ["created_at", "updated_at"]:
-                obj.__setitem__(key, value.isoformat())
-                continue
-            obj[key] = value
-
-        return obj
+        """Returns a dictionary containing all keys/values of __dict__ of the
+        instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
