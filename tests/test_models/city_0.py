808f490f1503b6e1d835a170776ba7719b32e82b
blessiingab] <b.ingabire1@alustudent.com>
2025-09-14 19:29:08 +0200

    provide solutions to the questions

--- tests/test_models/city_0.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Test file for City class"""
-from models.city import City
-
-city = City()
-print("OK" if type(city) is City else "Fail")
--- tests/test_models/place_0.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Test file for Place class"""
-from models.place import Place
-
-place = Place()
-print("OK" if type(place) is Place else "Fail")
